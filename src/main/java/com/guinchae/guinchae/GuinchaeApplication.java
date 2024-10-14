51fb43bf4697f580679405b7f8a30060b733e6b7
Miguel Silvano <miguelsilvano.20@hotmail.com>
2024-10-14 15:30:34 -0300

    Primeiro commit

---
+++ src/main/java/com/guinchae/guinchae/GuinchaeApplication.java
@@ -0,0 +1,11 @@
+package com.guinchae.guinchae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuinchaeApplication {
+
+	public static void main(String[] args) {	SpringApplication.run(GuinchaeApplication.class, args);	}
+
+}
